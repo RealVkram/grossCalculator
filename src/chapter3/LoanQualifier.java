9d7f6d18c63129ad96259505da6fa732e1ad3815
RealVkram <ajayiadegbemi@gmail.com>
2019-12-18 11:07:40 +0100

    loan calculator for employess

---
+++ src/chapter3/LoanQualifier.java
@@ -0,0 +1,39 @@
+package chapter3;
+
+import java.util.Scanner;
+
+/*
+***to qualify, you must be making about $30,000 / year
+* must have worked about 2 or more years
+ */
+public class LoanQualifier {
+    public static void main(String args[]) {
+        //initialize the known
+        int requiredSalary = 30000;
+        int requiredYearsEmployed = 2;
+
+        // get the unknown
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your salary: ");
+        double salary = scanner.nextDouble();
+
+        //get the number of years worked
+        System.out.print("How many years have you been in your recent employment ");
+        double yearsEmployed = scanner.nextDouble();
+        scanner.close();
+
+        if (salary >= requiredSalary){
+            if (yearsEmployed >= requiredYearsEmployed) {
+                System.out.println("Congrats, You qualify for the loan");
+            }
+            else {
+                System.out.println("I am sorry, You need to be in your recent employment for about " + requiredYearsEmployed + " or more in " +
+                        "order to qualify");
+            }
+        }
+        else {
+            System.out.println("I am sorry, you need to make about N" + requiredSalary + " or more to qualify");
+        }
+
+    }
+}
