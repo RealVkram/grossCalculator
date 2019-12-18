b9e104b4ff8bd166efd382f44ffd529425974c66
RealVkram <ajayiadegbemi@gmail.com>
2019-12-18 16:30:05 +0100

    test grade decider

---
+++ src/chapter3/TestResults.java
@@ -0,0 +1,33 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class TestResults {
+    public static void main(String args[]) {
+
+        // get the test results
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your score: ");
+        double score = scanner.nextDouble();
+
+        //check the result to determine the grade
+        char grade;
+        if (score < 60) {
+            grade = 'F';
+        }
+        else if (score < 70) {
+            grade = 'D';
+        }
+        else if (score < 80) {
+            grade = 'C';
+        }
+        else if (score < 90) {
+            grade = 'B';
+        }
+        else {
+            grade = 'A';
+        }
+        System.out.println("Your test grade is " + grade);
+    }
+
+}
