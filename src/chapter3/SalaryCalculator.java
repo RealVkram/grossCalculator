9239f94ab1f408ef327f7368812ba518665b1248
RealVkram <ajayiadegbemi@gmail.com>
2019-12-18 09:02:38 +0100

    update with the condition for sales exceeding sales target of 10

---
+++ src/chapter3/SalaryCalculator.java
@@ -0,0 +1,30 @@
+package chapter3;
+
+/*all salespeople get a payment of $1000 a week
+ Salespeople who exceed 10 sales get an additional bonus of $250
+ */
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+    public static void main(String arg[]) {
+        //initialize the given values
+        int salary = 1000;
+        int bonus = 250;
+        int salesTarget = 10;
+
+        //get the unknown
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of the employee: ");
+        String name = scanner.next();
+
+        System.out.print("How many sales did " + name + " made this week? ");
+        int sales = scanner.nextInt();
+
+        //check the sales if it exceeds the 10
+        if (sales > salesTarget) {
+            salary += bonus;
+        }
+        System.out.println(name + " made N" + salary + " this month");
+    }
+}
